bba0939a538ceadbea7e9ab33c3ffef67749906b
Leonard Techel <ltechel@fork.de>
2018-11-10 15:31:13 +0100

    📄 page: redirect to editor on 404, full-page caching for performance++

--- page_handler.go
+++ page_handler.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/patrickmn/go-cache"
 	"html/template"
+	"io"
 	"net/http"
 	"path"
 )
@@ -22,12 +25,25 @@ func (b *Bilbo) HandlePage(w http.ResponseWriter, r *http.Request) {
 
 	page, commit, err := b.getRequestedPage(r)
 	if err != nil {
-		panic(err)
+		if err == io.EOF {
+			redirectUrl, _ := b.mux.Get("pages#edit").URL("page", normalizePageLink(vars["page"], true))
+			http.Redirect(w, r, redirectUrl.String(), http.StatusSeeOther)
+		} else {
+			panic(err)
+		}
 	}
 
-	err = b.RenderPage(page, commit)
-	if err != nil {
-		panic(err)
+	cacheKey := fmt.Sprintf("page-%s-%s", page.Filepath, commit)
+	cachedPage, found := b.cache.Get(cacheKey)
+	if found {
+		page = cachedPage.(*Page)
+	} else {
+		err = b.RenderPage(page, commit)
+		if err != nil {
+			panic(err)
+		}
+
+		b.cache.Set(cacheKey, page, cache.DefaultExpiration)
 	}
 
 	pageFolder := path.Dir(page.Linkpath)
